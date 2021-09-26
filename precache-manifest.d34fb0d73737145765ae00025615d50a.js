8deb1072d118f06a0cf994e684df91e5633b2012
Julio-Freitas <Julio-Freitas@users.noreply.github.com>
2021-09-26 16:12:20 +0000

    deploy: 0d0325477060122178cb7451b8be918fd3a238a8

---
+++ precache-manifest.d34fb0d73737145765ae00025615d50a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "670ba6c593f5ec89770de189b79063b9",
+    "url": "/breakingbadapi/index.html"
+  },
+  {
+    "revision": "ddd18ac364cbab869a6c",
+    "url": "/breakingbadapi/static/css/main.906d035d.chunk.css"
+  },
+  {
+    "revision": "132609558d3a87c6d895",
+    "url": "/breakingbadapi/static/js/2.afb74218.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/breakingbadapi/static/js/2.afb74218.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddd18ac364cbab869a6c",
+    "url": "/breakingbadapi/static/js/main.0575b0cd.chunk.js"
+  },
+  {
+    "revision": "c55dca291fbcf06689ca",
+    "url": "/breakingbadapi/static/js/runtime-main.36353e4f.js"
+  },
+  {
+    "revision": "78d8a85ca5b04456831442bce4bcbf35",
+    "url": "/breakingbadapi/static/media/bb.78d8a85c.png"
+  }
+]);
