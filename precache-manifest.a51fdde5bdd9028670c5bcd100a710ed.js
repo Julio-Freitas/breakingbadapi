26b7145fc131e81432f1e9c9859042ea42e336cc
Julio-Freitas <julio.clf31@gmail.com>
2021-09-26 09:29:28 -0300

    Updates

---
+++ precache-manifest.a51fdde5bdd9028670c5bcd100a710ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2e5d0c27790969061da0689fc7af1d4",
+    "url": "/breakingbadapi/index.html"
+  },
+  {
+    "revision": "a78e973e4e77ddac43ea",
+    "url": "/breakingbadapi/static/css/main.906d035d.chunk.css"
+  },
+  {
+    "revision": "a603a63d3d2d30d464a8",
+    "url": "/breakingbadapi/static/js/2.66be39f3.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/breakingbadapi/static/js/2.66be39f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a78e973e4e77ddac43ea",
+    "url": "/breakingbadapi/static/js/main.705beaa3.chunk.js"
+  },
+  {
+    "revision": "c55dca291fbcf06689ca",
+    "url": "/breakingbadapi/static/js/runtime-main.36353e4f.js"
+  },
+  {
+    "revision": "78d8a85ca5b04456831442bce4bcbf35",
+    "url": "/breakingbadapi/static/media/bb.78d8a85c.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.07e4cbfe7354ac8c13311ea8e80cc9c6.js"
+  "/breakingbadapi/precache-manifest.a51fdde5bdd9028670c5bcd100a710ed.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/breakingbadapi/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
